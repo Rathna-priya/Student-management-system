a95680616c92d8251090d0d2085206093b11e431
Rathna-priya <155623669+Rathna-priya@users.noreply.github.com>
2025-01-16 17:25:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+import supabase from "./config/supabase";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./screens/Home";
+import Create from "./screens/Create";
+import Navbar from "./components/Navbar";
+import { Container } from "@mui/material";
+import Update from "./screens/Update";
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Container sx={{marginTop:5}}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/:id" element={<Update/>}/>
+          </Routes>
+        </Container>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
